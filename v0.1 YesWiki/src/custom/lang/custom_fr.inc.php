29253410a8953259d28adfb5341d1fc4e8f21586
Jérémy Dufraisse <67152407+J9rem@users.noreply.github.com>
2020-12-12 21:32:20 +0100

    preparing update to doryphore 2020-11-30

---
+++ v0.1 YesWiki/src/custom/lang/custom_fr.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Mise à jour du nom des bandeaux pour CdlT
+*
+*/
+
+$GLOBALS['translations'] = array_merge(
+    $GLOBALS['translations'],
+    array(
+        'BAZ_VERIFIER_MON_ADRESSE' => 'Cliquez sur ce bandeau pour positionner votre adresse sur la carte'
+    )
+);
