1d9de7451e1e21d0d16079963fa378d67891b1d9
Jérémy Dufraisse <67152407+J9rem@users.noreply.github.com>
2020-12-14 22:48:52 +0100

    first test for custom import csv

---
+++ v0.1 YesWiki/src/custom/lang/custom_en.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$GLOBALS['translations'] = array_merge(
+    $GLOBALS['translations'],
+    array(
+    'CUSTOM_ACTION_IMPORTCDLT' => 'Action {{importcdlt ...}}',
+    'CUSTOM_ACTION_IMPORTCDLT_ONLY_FOR_CONNECTED' => 'the action is reserved for connected people',
+    )
+);
---
+++ v0.1 YesWiki/src/custom/lang/custom_fr.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$GLOBALS['translations'] = array_merge(
+    $GLOBALS['translations'],
+    array(
+    'CUSTOM_ACTION_IMPORTCDLT' => 'Action {{importcdlt ...}}',
+    'CUSTOM_ACTION_IMPORTCDLT_ONLY_FOR_CONNECTED' => 'l\'action est r&eacute;serv&eacute;e aux personnes connect&acute;',
+    )
+);
