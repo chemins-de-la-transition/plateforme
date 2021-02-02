f132ad1d1061d5a4de34e56957ed7b6232a236ba
Jérémy Dufraisse <67152407+J9rem@users.noreply.github.com>
2021-02-02 22:17:24 +0100

    update for Dory 2021 02 01 -1

--- v0.1 YesWiki/src/custom/handlers/formEditTemplateCdlt.php
+++ v0.1 YesWiki/src/custom/handlers/formEditTemplateCdlt.php
@@ -14,20 +14,37 @@ if (!defined("WIKINI_VERSION")) {
 }
 
 // Lit une page web dans un tableau.
-$file = file_get_contents('/home/leschemisi/www/plateforme/tools/bazar/presentation/javascripts/form-edit-template.js');
+$file = file_get_contents(__DIR__ . '/../../tools/bazar/presentation/javascripts/form-edit-template.js');
+$new_str  = '';
+if ($this->userIsAdmin()) {
+  $groupsNames = $this->GetGroupsList() ;
+  foreach($groupsNames as $groupName) {
+    if ($groupName != 'admins') {
+        $new_str .= '  "@' . $groupName . '": "Membre du groupe ' . $groupName . '",'."\n";
+        $new_str .= '  "% @' . $groupName . '": "Membre du groupe ' . $groupName . ', propriétaire et admins",'."\n";
+    }
+  }
+  foreach($groupsNames as $groupName) {
+    if ($groupName != 'admins') {
+        $new_str .= '  "!@' . $groupName . '": "Non membre du groupe ' . $groupName . '",'."\n";
+    }
+  }
+} 
 
 header('Content-Type: text/javascript');
 // Remplace les valeurs locales
-$anchor_str = '"@admins": "Membre du groupe admin",'."\n"; 
-$new_str  ='  "@moderateurs": "Membre du groupe moderateurs",'."\n" ;
-$new_str .='  "!@moderateurs": "Non membre du groupe moderateurs",'."\n" ;
-$new_str .='  "% @moderateurs": "Membre du groupe moderateurs, propriétaire et admins",'."\n" ;
-$new_str .='  "@editeurs": "Membre du groupe éditeurs",'."\n" ;
-$new_str .='  "@Lieux": "Membre du groupe Lieux",'."\n";
+$anchor_base_str = '"@admins": "Membre du groupe admin",';
+
+$anchor_str = $anchor_base_str."\n"; 
+$file = str_replace($anchor_str,$anchor_str.$new_str,$file) ;
+$anchor_str = $anchor_base_str."\r\n"; 
 $file = str_replace($anchor_str,$anchor_str.$new_str,$file) ;
 // if no coma
-$anchor_str = '"@admins": "Membre du groupe admin"'."\n";
+$anchor_base_str = '"@admins": "Membre du groupe admin"';
 $new_str = ',' . $new_str ;
+$anchor_str = $anchor_base_str."\n"; 
+$file = str_replace($anchor_str,$anchor_str.$new_str,$file) ;
+$anchor_str = $anchor_base_str."\r\n"; 
 $file = str_replace($anchor_str,$anchor_str.$new_str,$file) ;
 // keywords and query mapping
 $anchor_str  = '  12: "write",'."\n";
@@ -36,6 +53,9 @@ $new_str  = '  12: "write",'."\n";
 $new_str .= '  13: "keywords",'."\n";
 $new_str .= '  14: "semantic",'."\n";
 $new_str .= '  15: "queries",'."\n";
+$file = str_replace($anchor_str,$new_str,$file) ;
+// with \r\n
+$anchor_str = str_replace("\n","\r\n",$anchor_str) ;
 $file = str_replace($anchor_str,$new_str,$file) ;
 // resolve trouble for queries
 $anchor_str  = '      fillingMode: {'."\n";
@@ -50,6 +70,9 @@ $new_str .= '        label: "Critères de filtre",'."\n";
 $new_str .= '        placeholder: "radioListeOuiNonbf_officiel=oui",'."\n";
 $new_str .= '      },'."\n";
 $file = str_replace($anchor_str,$anchor_str.$new_str,$file) ;
+// with \r\n
+$anchor_str = str_replace("\n","\r\n",$anchor_str) ;
+$file = str_replace($anchor_str,$anchor_str.$new_str,$file) ;
 // new choice
 $anchor_str  = '      fillingMode: {'."\n";
 $anchor_str .= '        label: "Mode de saisie",'."\n";
@@ -59,6 +82,9 @@ $anchor_str .= '          tags: "En Tags",'."\n";
 $anchor_str .= '          dragndrop: "Drag & drop",'."\n";
 $anchor_str .= '        },'."\n";
 $anchor_str .= '      },'."\n";
+$file = str_replace($anchor_str,$anchor_str.$new_str,$file) ;
+// with \r\n
+$anchor_str = str_replace("\n","\r\n",$anchor_str) ;
 $file = str_replace($anchor_str,$anchor_str.$new_str,$file) ;
 echo $file ;
 
