3499604352586e2655f1626182e02b7cec1d7dfd
Jérémy Dufraisse <67152407+J9rem@users.noreply.github.com>
2021-03-02 11:02:11 +0100

    #243 NomWiki détails

---
+++ v0.1 YesWiki/src/custom/actions/__login.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Mise à jour du nom des bandeaux pour CdlT
+*
+*/
+
+$GLOBALS['translations'] = array_merge(
+    $GLOBALS['translations'],
+    array(
+        'LOGIN_WIKINAME' => 'Email ou identifiant (NomWiki)'
+    )
+);
---
+++ v0.1 YesWiki/src/custom/actions/__usersettings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Mise à jour du nom des bandeaux pour CdlT
+*
+*/
+
+$GLOBALS['translations'] = array_merge(
+    $GLOBALS['translations'],
+    array(
+        'USER_WIKINAME' => 'Votre identifiant (ex. : NomWiki ; au moins 2 majuscules, pas d\'espaces, pas de caractères spéciaux)',
+        'USERNAME_MUST_BE_WIKINAME' => 'Votre nom d\'utilisateur doit &ecirc;tre format&eacute; en NomWiki (ex: CapitaineHaddock).',
+    )
+);
