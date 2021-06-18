f609a05b649c58a8c830c25d799dd79014baadb5
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-06-18 12:05:42 +0200

    fix(replacebazarjs): add bazar.js if not present

--- v0.1 YesWiki/src/custom/fields/replacebazarjsField.php
+++ v0.1 YesWiki/src/custom/fields/replacebazarjsField.php
@@ -18,6 +18,8 @@ class replacebazarjsField extends BazarField
     
     protected function renderInput($entry)
     {
+        // to be sure to have bazar.js before rendering
+        $this->getService(Wiki::class)->AddJavascriptFile('tools/bazar/libs/bazar.js');
         $pattern = '/(?<=\/)(tools\/bazar\/libs\/bazar\.js(?:\?v=[0-9-]*)?)(?="><\/script>)/';
         $replacement = '?' . $this->getService(Wiki::class)->config['root_page'] . '/bazarForCdlT';
         $GLOBALS['js'] = preg_replace($pattern, $replacement, $GLOBALS['js']);
