22368df3e612acf963e9e342d762ff5212258e7c
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-06-18 11:48:22 +0200

    revert(bazar__): revert to use templates

--- v0.1 YesWiki/src/custom/actions/bazar__.php
+++ v0.1 YesWiki/src/custom/actions/bazar__.php
@@ -1,7 +1,14 @@
 <?php
-
 if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-echo $this->Action('bazarpost',0,array_merge($this->parameter,['calledBy' => 'bazar__']));
+/**
+* Remplacement du javascript par défaut
+**/
+
+if ($this->UserIsAdmin()){
+    $pattern = '/(?<=\/)(tools\/bazar\/presentation\/javascripts\/form-edit-template\.js(?:\?v=[0-9-]*)?)(?="><\/script>)/';
+    $replacement = '?' . $this->config['root_page'] . '/formEditTemplateCdlt';
+    $GLOBALS['js'] = preg_replace($pattern,$replacement,$GLOBALS['js']);
+}
